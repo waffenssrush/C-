c9b3612dfc5db9e30c9adc1fc725f0daf9d7a505
waffenssrush <52982493+waffenssrush@users.noreply.github.com>
2021-11-29 21:44:30 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("情输入用户名");
+            Console.ReadLine();
+            Console.WriteLine("请输入年龄");
+            Console.ReadLine();
+            Console.WriteLine("please input your classroom");
+            Console.ReadLine();
+
+            Console.WriteLine("what sports do you like?");
+            Console.ReadLine();
+            Console.WriteLine("haha,lucky,i love it too");
+
+            Console.WriteLine("**********\n*        *\n*        *\n*        *\n*        *\n*        *\n*        *\n*        *\n*        *\n*        *\n**********");
+        }
+    }
+}
